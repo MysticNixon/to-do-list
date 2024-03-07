86056bd292dcff7b31b528ff89d35b5a743b80eb
MysticNixon <nikoloz.sherozia08@gmail.com>
2024-03-07 17:40:26 +0400

    update git ignore

---
+++ src/app/addressBook/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import styles from './page.module.css'
+import React, { useState, Dispatch, SetStateAction } from 'react'
+
+export default function Home() {
+
+
+  return (
+    <main className={[styles.main, 'center'].join(" ")}>
+<div>
+    <p>vaxo</p>
+    <p>555352360</p>
+</div>
+<div>
+    <p>zaza</p>
+    <p>555342360</p>
+</div>
+    </main>
+  )
+}
