bd8f0c01509c5e3321f3ff770f1f6ac788acff20
MysticNixon <nikoloz.sherozia08@gmail.com>
2024-03-15 21:18:43 +0400

    addressbook changes

--- src/app/addressBook/page.tsx
+++ src/app/addressBook/page.tsx
@@ -1,20 +1,42 @@
 'use client'
 import styles from './page.module.css'
 import React, { useState, Dispatch, SetStateAction } from 'react'
+import data from "../../../assets/min-data.json";
 
 export default function Home() {
 
+  function addZero(piradinomeri: number) {
+    const stringZero = "0"
+    let piradinomeriButString = "" + piradinomeri
+    let concat = stringZero.concat(piradinomeriButString)
+    if (isTenDiget(piradinomeri) === true) {
+      return concat
+    }
+    else {
+      return piradinomeriButString
+    }
+  }
+
+  function isTenDiget(piradinomeri: number): boolean {
+    if (piradinomeri < 10000000000) {
+      return true
+    }
+    if (piradinomeri >= 1000000000) {
+      return true
+    }
+    return false
+  }
 
   return (
     <main className={[styles.main, 'center'].join(" ")}>
-<div>
-    <p>vaxo</p>
-    <p>555352360</p>
-</div>
-<div>
-    <p>zaza</p>
-    <p>555342360</p>
-</div>
+      {data.contactInfo.map((value, index) => {
+        return (
+          <div key={value["piadi#"]}>
+            <p>{value.saxeli} {value.gvari}</p>
+            <p>{addZero(value['piadi#'])}</p>
+          </div>
+        )
+      })}
     </main>
   )
 }
